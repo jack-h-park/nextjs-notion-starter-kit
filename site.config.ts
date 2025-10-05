84dc9b575f76c45c293a56513cf97249388b24b6
Jack H. Park (MacBook) <jackpark.kr@gmail.com>
2025-10-04 23:12:54 -0700

    baseline setup done

--- site.config.ts
+++ site.config.ts
@@ -51,16 +51,12 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+     {
+       title: 'About',
+       pageId: '28299029c0b4816e89c0c4f17a39963b'
+     }
+  ]
 })
