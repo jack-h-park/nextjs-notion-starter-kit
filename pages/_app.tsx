27c33dfd66ac54ab4233e233218b06ec37354849
Jack H. Park (MacBook) <jackpark.kr@gmail.com>
2025-10-26 12:23:36 -0700

    feat: Google Analytics support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,36 @@ import {
   posthogId
 } from '@/lib/config'
 
+// Google Analytics
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  window.gtag?.('config', GA_TRACKING_ID, {
+    page_path: url
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag?.('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  })
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   useEffect(() => {
+    // Google Analytics
+    const handleRouteChange = (url) => {
+      if (GA_TRACKING_ID) {
+        pageview(url)
+      }
+    }
+
     function onRouteChangeComplete() {
       if (fathomId) {
         Fathom.trackPageview()
@@ -39,9 +65,11 @@ export default function App({ Component, pageProps }: AppProps) {
     }
 
     router.events.on('routeChangeComplete', onRouteChangeComplete)
+    router.events.on('routeChangeComplete', handleRouteChange)
 
     return () => {
       router.events.off('routeChangeComplete', onRouteChangeComplete)
+      router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [router.events])
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,27 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+
+            {process.env.NEXT_PUBLIC_GA_ID && (
+              <>
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                        page_path: window.location.pathname,
+                      });
+                    `
+                  }}
+                />
+              </>
+            )}
           </Head>
 
           <body>
